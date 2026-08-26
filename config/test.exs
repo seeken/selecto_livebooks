42a85134119e4a0402e720753e63613480b7f074
Chris Rohlfs <seeken@gmail.com>
2026-08-25 19:45:06 -0600

    Allow remote database-backed Livebook tests

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,13 @@
 import Config
 
 config :selecto_livebooks, SelectoLivebooks.Repo,
-  database: "selecto_livebooks_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database:
+    System.get_env("SELECTO_LIVEBOOKS_DB") ||
+      "selecto_livebooks_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: System.get_env("SELECTO_LIVEBOOKS_DB_USER", "postgres"),
+  password: System.get_env("SELECTO_LIVEBOOKS_DB_PASS", "postgres"),
+  hostname: System.get_env("SELECTO_LIVEBOOKS_DB_HOST", "localhost"),
+  port: String.to_integer(System.get_env("SELECTO_LIVEBOOKS_DB_PORT", "5432")),
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :logger, level: :warning
