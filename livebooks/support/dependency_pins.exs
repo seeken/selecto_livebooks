699b9fc43d7ca7ddf213ed506731d8dd1f7773b1
Chris Rohlfs <seeken@gmail.com>
2026-09-04 16:00:22 -0600

    Refresh Selecto dependency pins

--- livebooks/support/dependency_pins.exs
+++ livebooks/support/dependency_pins.exs
@@ -2,15 +2,15 @@
 %{
   selecto: [
     git: "git@github.com:seeken/selecto.git",
-    ref: "40091a90618c5ea954f3e4c147a5be2202d78ecd"
+    ref: "b8b60cc537bd6194c5eb1568d2bee029230b5f86"
   ],
   selecto_db_postgresql: [
     git: "git@github.com:seeken/selecto_db_postgresql.git",
-    ref: "39c826831a6512f8f41172cc2722f50d5389e322"
+    ref: "ac9ce57d6b5c52e34af1fc30d8e026e890b10043"
   ],
   selecto_updato: [
     git: "git@github.com:seeken/selecto_updato.git",
-    ref: "592a2954579cf303ed10b4f31741179b39618ced"
+    ref: "a5759de0206dc86b026fbaa835d50d4f7332e9a9"
   ],
   selecto_components: [
     git: "git@github.com:seeken/selecto_components.git",
